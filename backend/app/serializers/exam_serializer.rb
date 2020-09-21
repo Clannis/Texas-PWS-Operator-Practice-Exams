0e68d59730c55dfe8d219b13959bd1c10f83394e
Clannis <Dodds1990@gmail.com>
2020-09-21 01:16:41 -0500

    update serializers to pass questions with exam in rendered json response

--- backend/app/serializers/exam_serializer.rb
+++ backend/app/serializers/exam_serializer.rb
@@ -2,4 +2,5 @@ class ExamSerializer < ActiveModel::Serializer
   attributes :id, :license, :field, :grade, :created_at
 
   belongs_to :user
+  has_many :questions
 end
--- backend/app/serializers/question_serializer.rb
+++ backend/app/serializers/question_serializer.rb
@@ -1,3 +1,3 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :prompt, :answer_a, :answer_b, :answer_c, :answer_d, :answer_e, :answer_f, :multiple_answers, :difficulty_rating, :category
+  attributes :id, :prompt, :a, :b, :c, :d, :e, :f, :multiple_answers, :difficulty_rating, :category
 end
