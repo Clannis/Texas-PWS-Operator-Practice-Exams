ef2e688af45c9f0ce1d73f8cc324f53296746ef4
Clannis <Dodds1990@gmail.com>
2020-09-20 22:13:10 -0500

    add method to exam to randomly populate questions for that exam instance

--- backend/app/models/exam.rb
+++ backend/app/models/exam.rb
@@ -1,4 +1,15 @@
 class Exam < ApplicationRecord
   belongs_to :user
   has_many :questions
+
+  def populate_questions
+    while self.questions.length < 50
+      id = rand(1..Question.all.last.id)
+      question = Question.find(id)
+      if question && !self.questions.include?(questoin)
+          self.questions << question
+      end
+    end
+  end
+  
 end
