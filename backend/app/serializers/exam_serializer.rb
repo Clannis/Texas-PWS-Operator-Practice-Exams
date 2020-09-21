a20db63149b17620932ebef706fd4734f08eb068
Clannis <Dodds1990@gmail.com>
2020-09-21 16:21:28 -0500

    add completed and current_question attributes to exam serializer

--- backend/app/serializers/exam_serializer.rb
+++ backend/app/serializers/exam_serializer.rb
@@ -1,5 +1,5 @@
 class ExamSerializer < ActiveModel::Serializer
-  attributes :id, :license, :field, :grade, :created_at
+  attributes :id, :license, :field, :grade, :created_at, :completed, :current_question
 
   belongs_to :user
   has_many :questions
